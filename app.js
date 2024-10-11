aea55d2cb1078003c575c93f9e9f667ded1a541d
Vektor <h269368@stud.u-szeged.hu>
2024-10-11 10:47:47 +0200

    add: basic app file with routes

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const indexrouter = require("./routes/index");
+const gamerouter = require("./routes/game");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use("/", indexrouter);
+app.use("/game", gamerouter);
+app.use("/css", express.static("./public/css"));
+app.use("/threejs", express.static("./public/threejs"));
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
