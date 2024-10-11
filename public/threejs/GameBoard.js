332d8f8d8c182f56a19e9dc3c5c2346bd9bd45d2
Vektor <h269368@stud.u-szeged.hu>
2024-10-11 13:33:26 +0200

    chore: whitespace differences

--- public/threejs/GameBoard.js
+++ public/threejs/GameBoard.js
@@ -7,15 +7,15 @@ function init() {
     //set up scene and camera
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000,
-);
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000,
+    );
     camera.position.z = 10;
     renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
     let texture = new THREE.TextureLoader().load("../textures/wood.jpg");
     let material = new THREE.MeshBasicMaterial({ map: texture });
