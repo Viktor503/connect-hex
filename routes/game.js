767d67919cfe2925f6ca1817f89cece09ee26a52
Szabó Viktor <szabo.viktor@mozaweb.com>
2025-03-28 16:02:35 +0100

    add: router to get online game

--- routes/game.js
+++ routes/game.js
@@ -2,7 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    res.render("game", { board_size: req.body.board_size, hex_mode: req.body.hex_mode });
+    res.render("game", {
+        board_size: req.body.board_size,
+        hex_mode: req.body.hex_mode,
+    });
+});
+
+router.get("/:id", async (req, res) => {
+    const roomId = req.params.id;
+    const roomInfo = req.socketManager.getRoomInfo(roomId);
+    if (roomInfo) {
+        console.log(roomInfo);
+        res.render("game_online", roomInfo["gameConfig"]);
+    } else {
+        res.send("404");
+    }
+    // res.render("game", {
+    //     board_size: req.body.board_size,
+    //     hex_mode: req.body.hex_mode,
+    //     room: req.body.room,
+    // });
 });
 
 module.exports = router;
