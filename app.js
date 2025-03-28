39fa4e100df4899e91b1dc1f2d64f9b3b170c0c8
Szabó Viktor <szabo.viktor@mozaweb.com>
2025-03-28 15:43:49 +0100

    add: socket.io server in app.js

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ const express = require("express");
 const indexrouter = require("./routes/index");
 const gamerouter = require("./routes/game");
 const bodyParser = require("body-parser");
+const socketIo = require("socket.io");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
+//const io = socketIo(server);
+
+//const rooms = {};
+
+const socketManager = require("./public/socketio/socketio_manage.js");
+socketManager.initialize(server);
 
 const PORT = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use("/", indexrouter);
-app.use("/game", gamerouter);
+app.use(
+    "/game",
+    (req, res, next) => {
+        req.socketManager = socketManager;
+        next();
+    },
+    gamerouter,
+);
 app.use("/css", express.static("./public/css"));
 app.use("/threejs", express.static("./public/threejs"));
 app.use("/textures", express.static("./public/textures"));
 app.use("/models", express.static("./public/models"));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
