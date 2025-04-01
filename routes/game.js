c7de356e5010f419166fdaca3bebf767af1e53d1
Szabó Viktor <szabo.viktor@mozaweb.com>
2025-04-01 14:37:00 +0200

    add: path for game with ai

--- routes/game.js
+++ routes/game.js
@@ -8,6 +8,28 @@ router.post("/", async (req, res) => {
     });
 });
 
+router.post("/ai", async (req, res) => {
+    console.log(req.body.model);
+    let player_order;
+    if (req.body.player_order_ai == "me") {
+        player_order = 1;
+    } else if (req.body.player_order_ai == "opponent") {
+        player_order = 2;
+    } else {
+        player_order = Math.floor(Math.random() * 2) + 1;
+    }
+    ai_order = player_order === 1 ? 2 : 1;
+
+    res.render("game_ai", {
+        board_size: req.body.board_size_ai,
+        hex_mode: req.body.hex_mode_ai,
+        player_order: player_order,
+        ai_model: req.body.model,
+    });
+});
+
+module.exports = router;
+
 router.get("/:id", async (req, res) => {
     const roomId = req.params.id;
     const roomInfo = req.socketManager.getRoomInfo(roomId);
