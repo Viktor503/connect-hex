d84e912ad2de2af6b14c8a47e9969b8d1fb16836
Szabó Viktor <szabo.viktor@mozaweb.com>
2025-04-26 14:37:35 +0200

    change: reformat random_model code

--- public/ai_models/random_model.js
+++ public/ai_models/random_model.js
@@ -1,26 +1,35 @@
 const { BaseModel } = require("./base_model");
 
 class randomModel extends BaseModel {
-    constructor(playerOrder, hexMode) {
-        super(playerOrder, hexMode);
-    }
-
     predict(gameState) {
+        console.log(gameState);
         console.log("predicting");
         if (this.hexMode) {
-            let x = Math.floor(Math.random() * gameState.length);
-            let y = Math.floor(Math.random() * gameState.length);
+            let x = this.randomInt(gameState.length);
+            let y = this.randomInt(gameState[0].length);
             while (gameState[x][y] != 0) {
-                x = Math.floor(Math.random() * gameState.length);
-                y = Math.floor(Math.random() * gameState.length);
+                x = this.randomInt(gameState.length);
+                y = this.randomInt(gameState[0].length);
             }
+            console.log("loopdy loop is over but hex");
             return [x, y];
         } else {
-            let x = Math.floor(Math.random() * gameState.length);
-            while (gameState[x][0] != 0)
-                x = Math.floor(Math.random() * gameState.length);
-            return [x, 0];
+            let usable_collumns = [];
+            for (let i = 0; i < gameState.length * 2 - 1; i++) {
+                const coords = this.columnToCoordinate(i + 1, gameState);
+                console.log(`coords x:${coords[0]} y:${coords[1]}`);
+                if (gameState[coords[0]][coords[1]] === 0) {
+                    usable_collumns.push(i + 1);
+                    console.log(`${i + 1} pushed`);
+                }
+            }
+            console.log(usable_collumns);
+            console.log("loopdy loop is over");
+            let x = this.randomColumn(usable_collumns);
+            console.log(`${x}, ${this.columnToCoordinate(x, gameState)}`);
+            return this.columnToCoordinate(x, gameState);
         }
     }
 }
+
 module.exports = { randomModel };
