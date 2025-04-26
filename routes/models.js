df3355a66795e9b73c34529b6c3257d2dd3430ba
Szabó Viktor <szabo.viktor@mozaweb.com>
2025-04-26 14:38:08 +0200

    add: greedy model to project

--- routes/models.js
+++ routes/models.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { randomModel } = require("../public/ai_models/random_model");
+const { greedyModel } = require("../public/ai_models/greedy_model");
 
 router.post("/random", async (req, res) => {
     let model = new randomModel(req.body.playerOrder, req.body.hexMode);
@@ -8,5 +9,13 @@ router.post("/random", async (req, res) => {
     result = model.predict(req.body.gameState);
     res.json(result);
 });
+
+router.post("/greedy", async (req, res) => {
+    let model = new greedyModel(req.body.playerOrder, req.body.hexMode);
+
+    result = model.predict(req.body.gameState);
+    res.json(result);
+});
+
 
 module.exports = router;
