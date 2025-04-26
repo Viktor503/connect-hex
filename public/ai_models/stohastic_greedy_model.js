eaed5c0d1e557025f7b00adfa5fbedbe41039e41
Szabó Viktor <szabo.viktor@mozaweb.com>
2025-04-26 14:40:28 +0200

    add: stohastic greedy model to project

---
+++ public/ai_models/stohastic_greedy_model.js
@@ -0,0 +1,53 @@
+const { Queue } = require("../models/Queue");
+const { greedyModel } = require("./greedy_model");
+
+class greedyModelStochastic extends greedyModel {
+    bestFieldFromDistanceTable(distanceTable, gameState, playerFields) {
+        let [hasStartField, hasEndField] = this.edgeFieldsInPlayerFields(
+            playerFields,
+            gameState,
+        );
+
+        console.log(hasStartField, hasEndField);
+
+        let min_dist = gameState.length ** 2;
+        let min_field = [];
+
+        for (const field in distanceTable) {
+            const coord = this.fieldName_to_listCoord(field);
+            if (this.isStartEdge(coord[0], coord[1])) {
+                if (distanceTable[field]["dist"] < min_dist) {
+                    min_dist = distanceTable[field]["dist"];
+                    min_field = [];
+                    min_field.push({
+                        [field]: distanceTable[field],
+                    });
+                } else if (distanceTable[field]["dist"] == min_dist) {
+                    min_field.push({
+                        [field]: distanceTable[field],
+                    });
+                }
+            }
+        }
+
+        const picked_dest = min_field[this.randomInt(min_field.length)];
+
+        if (Object.keys(picked_dest).length === 0) {
+            throw new Error("Didn't find optimal field");
+        }
+        let best_field;
+        if (Object.values(picked_dest)[0]["dist"] > 1) {
+            best_field = this.backtrackFieldFromDistanceTable(
+                distanceTable,
+                gameState,
+                Object.values(picked_dest)[0],
+                Object.keys(picked_dest)[0],
+            );
+        } else {
+            best_field = Object.keys(picked_dest)[0];
+        }
+        console.log("best field", best_field);
+        return this.fieldName_to_listCoord(best_field);
+    }
+}
+module.exports = { greedyModelStochastic };
